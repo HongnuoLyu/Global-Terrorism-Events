4cd8e1978e3026d1cb7d5d27f1d9bbe449aa624a
HongnuoLyu <90594706+HongnuoLyu@users.noreply.github.com>
2021-10-16 13:04:54 +0800

    Add files via upload

---
+++ Assignment2.js
@@ -0,0 +1,20 @@
+var vg_1 = "1+2.json";
+vegaEmbed("#mapline", vg_1).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_3 ="3.json";
+vegaEmbed("#line", vg_3).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_4 ="4.json";
+vegaEmbed("#area", vg_4).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+
+var vg_5 ="5.json";
+vegaEmbed("#bar2", vg_5).then(function(result) {
+ // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
